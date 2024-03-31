80edc2b3d78e27a7a22f4c10b3a7128fb10179e4
Emil Małańczak <kdddmalanczak@gmail.com>
2024-03-31 20:50:43 +0200

    feat: 🎸 add swagger integration

---
+++ apps/api/src/plugins/swagger.plugin.ts
@@ -0,0 +1,43 @@
+import type { FastifyDynamicSwaggerOptions } from "@fastify/swagger";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+import fp from "fastify-plugin";
+import { generateOpenApiDocument } from "trpc-openapi";
+
+import { appRouter } from "../root";
+
+export const openApiDocument = generateOpenApiDocument(appRouter, {
+  title: "Lumpik OpenAPI",
+  version: "1.0.0",
+  description: "OpenAPI compliant REST API built using tRPC with Fastify",
+  baseUrl: "http://localhost:3000/api",
+  tags: ["auth"],
+});
+
+export const swaggerPlugin = fp<FastifyDynamicSwaggerOptions>(
+  async (fastify) => {
+    fastify.get("/openapi.json", () => openApiDocument);
+
+    await fastify.register(swagger, {
+      prefix: "/docs",
+      mode: "static",
+      specification: { document: openApiDocument },
+    });
+
+    await fastify.register(swaggerUi, {
+      routePrefix: "/docs",
+
+      uiConfig: {
+        docExpansion: "full",
+        deepLinking: false,
+        displayOperationId: true,
+      },
+      staticCSP: true,
+      transformStaticCSP: (header) => header,
+      transformSpecification: (swaggerObject) => {
+        return swaggerObject;
+      },
+      transformSpecificationClone: true,
+    });
+  },
+);
