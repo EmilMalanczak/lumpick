7c0547c2fc549d1d8eb4c3a608e8fe615c6b4f82
Emil Małańczak <kdddmalanczak@gmail.com>
2025-01-19 10:12:09 +0100

    Fix native app wrong trpc client url

--- apps/native/constants/settings.ts
+++ apps/native/constants/settings.ts
@@ -5,8 +5,8 @@ export const storageDomain = "https://www.example.com";
 const config = {
   dev: {
     apiUrl: __DEV__
-      ? `http://${Constants.expoConfig?.hostUri?.split(":")[0]}:3000`
-      : "https://api.example.com",
+      ? `http://${Constants.expoConfig?.hostUri?.split(":")[0]}:3000/trpc`
+      : "https://api.example.com", // TODO prod url,
   },
   prod: {
     apiUrl: "https://api.example.com",
